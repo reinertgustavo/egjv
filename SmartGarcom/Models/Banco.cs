1d41ce19b76ae907fe53722ab2a37fceafeb6ff0
Jaderson Jonas Silva <Jader_19_02@hotmail.com>
2019-06-04 20:27:17 -0300

    Início do projeto

---
+++ SmartGarcom/Models/Banco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartGarcom.Models
+{
+    public class Banco : DbContext
+    {
+        public Banco(DbContextOptions<Banco> options)
+            : base(options)
+        { }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
+
+
+
+
+
---
+++ SmartGarcom/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartGarcom.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
